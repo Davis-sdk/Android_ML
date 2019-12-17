d8aa5e34fcab18baac9a38421f7b02cd93116167
Davis-sdk <davizsdk@gmail.com>
2019-12-17 03:46:14 +0200

    Done

--- app/src/main/java/com/example/machine_learning_android/TensorMain.java
+++ app/src/main/java/com/example/machine_learning_android/TensorMain.java
@@ -2,14 +2,25 @@ package com.example.machine_learning_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.machine_learning_android.clasifier.Classifier;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
+
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -28,8 +39,8 @@ public class TensorMain extends AppCompatActivity {
 
 
     private Classifier classifier;
-    private Executer executer = Executors.newSingleThreadExecutor();
-    private TextView texViewResult;
+    private Executor executer = Executors.newSingleThreadExecutor();
+    private TextView textViewResult;
     private Button btnDetectObject, btnToggleCamera;
     private ImageView imageViewResult;
     private CameraView cameraView;
@@ -39,8 +50,140 @@ public class TensorMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initItems();
+
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+                imageViewResult.setImageBitmap(bitmap);
+                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+
+                textViewResult.setText(results.toString());
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+
+
+
+        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.toggleFacing();
+            }
+        });
+
+
+        btnDetectObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.captureImage();
+            }
+        });
+
+    initTensorflowAndLoadModel();
+
+
     }
 
-    itme
+    public void initItems()
+    {
+        cameraView = (CameraView) findViewById(R.id.cameraView);
+        imageViewResult = (ImageView) findViewById(R.id.imageViewResult);
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
+
+        btnToggleCamera = (Button) findViewById(R.id.btnToggleCamera);
+        btnDetectObject = (Button) findViewById(R.id.btnDetectObject);
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        cameraView.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
+    }
+
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        executer.execute(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                classifier.close();
+            }
+        });
+    }
+
+
+    private void initTensorflowAndLoadModel()
+    {
+        executer.execute(new Runnable ()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    classifier = TensorFlowImageClassifier.create(
+                            getAssets(),
+                            MODEL_FILE,
+                            LABEL_FILE,
+                            INPUT_SIZE,
+                            IMAGE_MEAN,
+                            IMAGE_STD,
+                            INPUT_NAME,
+                            OUTPUT_NAME);
+                        makeButtonVisible();
+                }catch (final Exception e)
+                {
+                    throw new RuntimeException("Error initializing TensorFlow", e);
+                }
+            }
+        });
+
+    }
+
+    private void makeButtonVisible()
+    {
+        runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                btnDetectObject.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
 
 }
